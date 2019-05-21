8d6cef487ed02fcbe521d82bb8a79109ba68c63a
Oleksandr Bakov <oleksandr.bakov@gmail.com>
2019-05-21 17:07:28 +0300

    react3.1

--- react_4/my-app/src/components/Controls/Controls.js
+++ react_4/my-app/src/components/Controls/Controls.js
@@ -1,22 +1,4 @@
 import React from 'react';
-import Home from '../Home/Home';
-import BookList from '../BookList/BookList';
-
-const Controls = ({ onChange, onSelect, onClick }) => (
-  <div>
-    <input onChange={onChange} class="input" type="text" placeholder="text" />
-    <select onChange={onSelect} class="select">
-      {genres.map(el => (
-        <option key={el} value={el}>
-          {el}
-        </option>
-      ))}
-    </select>
-    <button onClick={onClick} class="btn">
-      search
-    </button>
-  </div>
-);
 
 const genres = [
   'fantasy',
@@ -27,5 +9,26 @@ const genres = [
   'satire',
   'computers',
 ];
+
+const Controls = ({ onChange, onSelect, onClick }) => (
+  <div>
+    <input
+      onChange={onChange}
+      className="input"
+      type="text"
+      placeholder="text"
+    />
+    <select onChange={onSelect} className="select">
+      {genres.map(el => (
+        <option key={el} value={el}>
+          {el}
+        </option>
+      ))}
+    </select>
+    <button type="button" onClick={onClick} className="btn">
+      search
+    </button>
+  </div>
+);
 
 export default Controls;
